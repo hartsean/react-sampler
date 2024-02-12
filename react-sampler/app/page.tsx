5710475f7d0fe8a0447d11067fc18cb986471b5b
Sean Hart <hartsean@gmail.com>
2024-02-11 21:15:24 -0600

    install next.js

---
+++ react-sampler/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    
+      <div className="relative flex place-items-center">
+        <pre>
+          <code class="javascript">
+          toyProblems
+          </code>
+        </pre>
+      </div>
+
+    </main>
+  );
+}
